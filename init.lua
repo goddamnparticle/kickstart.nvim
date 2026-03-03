6ad04cc3f4f385a95c54061a112d10bdaa7977e7
Yusuf Salk <yusuf.salk@huawei.com>
2026-03-03 14:50:46 +0300

    make lazy ui rounded

--- init.lua
+++ init.lua
@@ -1095,6 +1095,10 @@ require('lazy').setup({
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    border = 'rounded',
+    -- draw border = 'rounded',
+
+
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
