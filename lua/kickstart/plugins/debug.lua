47cda0f344103dc885e249c57f22e00d2f286679
Yusuf Salk <yusuf.salk@huawei.com>
2026-05-03 22:05:38 +0300

    fix: debugpy correctly sets py environment

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -172,7 +172,9 @@ return {
           -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
           local cwd = vim.fn.getcwd()
 
-          if vim.env.CONDA_PREFIX and vim.fn.executable(vim.env.CONDA_PREFIX .. '/bin/python') == 1 then
+          if vim.env.VIRTUAL_ENV and vim.fn.executable(vim.env.VIRTUAL_ENV .. '/bin/python') == 1 then
+            return vim.env.VIRTUAL_ENV .. '/bin/python'
+          elseif vim.env.CONDA_PREFIX and vim.fn.executable(vim.env.CONDA_PREFIX .. '/bin/python') == 1 then
             return vim.env.CONDA_PREFIX .. '/bin/python'
           elseif vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
             return cwd .. '/venv/bin/python'
